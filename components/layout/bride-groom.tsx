c86897135abd961a23b2e6231879e9852aa5137a
thang1197 <thang.lehuu010197@gmail.com>
2025-09-14 21:47:03 +0700

    qwe

--- components/layout/bride-groom.tsx
+++ components/layout/bride-groom.tsx
@@ -1,33 +1,36 @@
 import { Heart } from "lucide-react";
 import Image from "next/image";
+import { Gwendolyn } from 'next/font/google'
+
+const gwendolyn = Gwendolyn({ subsets: ['latin'], weight: '700' });
 
 export function BrideGroom () {
     return (
       <section className="bg-[url('/bride-groom.jpg')] bg-no-repeat bg-cover bg-center w-full h-[1200px] py-10">
-         <div className="max-w-4xl mx-auto mt-10">
-          <div className="text-center mb-16 scroll-animate">
-            <h2 className="text-3xl font-light text-gray-800 mb-4 font-serif">
-              Trân Trọng Báo Tin{" "}
-              <span className="uppercase">Lễ THÀNH HÔN </span>
-              Của Con Chúng Tôi
+         <div className="max-w-4xl mx-auto mt-40">
+          <div className="text-center mb-8 scroll-animate">
+            <h2 className="text-3xl font-light text-gray-800 mb-2 font-serif">
+              Trân trọng báo tin
             </h2>
+            <p className="text-3xl text-gray-800 mb-2 font-serif uppercase font-bold">Lễ THÀNH HÔN </p>
+            <h2 className="text-3xl font-light text-gray-800 mb-2 font-serif">của con chúng tôi</h2>
             <div className="w-[70%] h-px bg-rose-400 mx-auto"></div>
 
-            <div className="my-5">
-              <div className="text-3xl py-2 tracking-widest">
-                <p>Lê Hữu Thắng</p>
-                <p>(Út Nam)</p>
+            <div className="mt-3">
+              <div className="py-1 tracking-widest">
+                <p className={`${gwendolyn.className} font-bold text-4xl`}>Lê Hữu Thắng</p>
+                <p className="font-serif">(Út Nam)</p>
               </div>
-              <div className="text-2xl py-2 flex items-center justify-center">
-                <Image src="/heart.png" alt="" width={80} height={80} />
+              <div className="text-2xl py-1 flex items-center justify-center">
+                <Image src="/heart.png" alt="" width={70} height={70} />
               </div>
-              <div className="text-3xl py-2 tracking-widest">
-                <p>Võ Thị Huế Anh</p>
-                <p>(Ái Nữ)</p>
+              <div className="py-2 tracking-widest">
+                <p className={`${gwendolyn.className} font-bold text-4xl`}>Võ Thị Huế Anh</p>
+                <p className="font-serif">(Ái Nữ)</p>
               </div>
             </div>
 
-            <div className="flex justify-center items-center gap-1">
+            <div className="flex justify-center items-center gap-1 py-1">
               <div>
                 <Image
                 src="/image1.jpg"
